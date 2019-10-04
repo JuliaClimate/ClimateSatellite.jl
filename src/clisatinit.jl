0dee1174537123f91377687dae24bb2ef64c2174
Nathanael Wong <natgeo.wong@outlook.com>
2019-10-04 01:16:13 -0400

    Added logging info, and FTP functionality for PMM Arthurhoe

--- src/clisatinit.jl
+++ src/clisatinit.jl
@@ -1,24 +1,46 @@
 """
 This file initializes the ClimateSatellite module by providing the basic
-functions that are applicable to all the different satellites, such as the root
-directory for which data is stored.
+functions that are applicable to all the different satellites.  Current
+functionalities include:
+    - detection of home directory
+    - ftp open for arthuhou
 
 """
 
+# Root Functions
 function clisatroot()
 
     svrdir = "/n/kuangdss01/users/nwong";
     dskdir = "/Volumes/CliNat-Sat")
     docdir = "/Users/natgeo-wong/Documents/Research/"
 
-    if     isdir(svrdir); return svrdir
+    if     isdir(svrdir); return svrdir;
+        info(logger,"The path $(svrdir) exists and therefore can be used as a directory for ClimateSatellite data downloads.")
     elseif isdir(dskdir); return dskdir
+        info(logger,"The path $(dskdir) exists and therefore can be used as a directory for ClimateSatellite data downloads.")
     elseif isdir(docdir); return docdir
-    else                  error(logger,"The predefined directories in clisatroot.jl do not exist.  They are user-dependent, so please modify/customize accordingly.")
+        info(logger,"The path $(docdir) exists and therefore can be used as a directory for ClimateSatellite data downloads.")
+    else
+        error(logger,"The predefined directories in clisatroot.jl do not exist.  They are user-dependent, so please modify/customize accordingly.")
     end
 
 end
 
 function clisatroot(path::AbstractString)
+    if isdir(path)
+        info(logger,"The path $(path) exists and therefore can be used as a directory for ClimateSatellite data downloads.")
+    end
     return path
+end
+
+# FTP Functions
+function pmmftpopen()
+    email = "natgeo.wong%40outlook.com"
+    info(logger,"Opening FTP request to arthurhou.pps.eosdis.nasa.gov.")
+    return FTP("ftp://$(email):$(email)@arthurhou.pps.eosdis.nasa.gov")
+end
+
+function ppmftpclose(ftp)
+    info(logger,"Closing FTP request to arthurhou.pps.eosdis.nasa.gov.")
+    close(ftp)
 end
