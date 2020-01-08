39d7fe571a2d54f8a470efac9f2b1900440f2146
natgeo-wong <natgeo.wong@outlook.com>
2020-01-08 18:31:26 -0500

    Added download file - to put in common download scripts here.

---
+++ src/download.jl
@@ -0,0 +1,6 @@
+"""
+This file contains all the scripts in ClimateSatellite.jl that are responsible for finding and downloading the data:
+    - detection of home directory
+    - ftp open for arthuhou and jsimpson (GPM / TRMM data)
+
+"""
