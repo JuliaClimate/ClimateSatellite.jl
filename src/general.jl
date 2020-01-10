3974474afb07128bfce654cacce8437e9420f00f
natgeo-wong <natgeo.wong@outlook.com>
2020-01-09 21:01:49 -0500

    Updated info text and possible download script calls.

--- src/general.jl
+++ src/general.jl
@@ -10,6 +10,19 @@ function clisatinfo(productID::AbstractString,productattr::Dict)
 
 end
 
+function clisatfol(info::Dict,date::TimeType,region::AbstractString)
+
+    fol = joinpath(info["root"],region,yr2str(date));
+
+    if !isdir(fol)
+        @info "$(Dates.now()) - $(info["satellite"]) $(info["product"]) data directory for the $(regionfullname(region)) region and year $(yr2str(date)) does not exist."
+        @info "$(Dates.now()) - Creating data directory $(fol)."; mkpath(fol);
+    end
+
+    return fol
+
+end
+
 function clisatdwn(
     year::Integer;
     productID::AbstractString, email::AbstractString,
@@ -18,26 +31,32 @@ function clisatdwn(
 
     if dataroot == ""; dataroot = clisatroot(product); end
 
-    info = Dict();
+    info = Dict("root"=>dataroot,"email"=>email);
     info = clisatinfo(productID,info);
 
-    if info["satellite"] = "GPM"
-        if     info["product"] = "early"; gpmedwn(regions,year,info,email);
-        elseif info["product"] = "late";  gpmldwn(regions,year,info,email);
-        elseif info["product"] = "final"; gpmfdwn(regions,year,info,email);
+    if info["source"] == "PMM"
+        if     isprod(info,"imerg");   gpmedwn(regions,year,info,email);
+        elseif isprod(info,"3b42");    trmmdwn(regions,year,info,email);
+        else;                          gpmndwn(regions,year,info,email);
         end
-    elseif info["satellite"] = "TRMM";    trmmdwn(regions,year,info,email);
-    elseif info["satellite"] = "MIMIC";   mtpwdwn(regions,year,info);
+    elseif info["source"] == "MIMIC";  mtpwdwn(regions,year,info);
+    elseif info["source"] == "RSS"
+        if     isprod(info,"trmm");    rtmidwn(regions,year,info,email);
+        elseif isprod(info,"gpm");     rgmidwn(regions,year,info,email);
+        elseif isprod(info,"smif");    rsmidwn(regions,year,info,email);
+        elseif isprod(info,"windsat"); rwnddwn(regions,year,info,email);
+        elseif isprod(info,"amsr");    rmsrdwn(regions,year,info,email);
+        end
     end
 
 end
 
 function clisatsave(
     data::Array{Real,3},
-    region::AbstractString, info::Dict, yrmo::AbstractString
+    region::AbstractString, info::Dict, date::TimeType
 )
 
-    fnc = clisatncfile(date,region,info);
+    fnc = clisatncfile(info,date,region);
     nlon = size(data,1); lon = info["longitude"];
     nlat = size(data,2); lat = info["latitude"];
     nt   = size(data,3);
@@ -64,11 +83,11 @@ function clisatsave(
     ncwrite(lon,fnc,var_lon);
     ncwrite(lat,fnc,var_lat);
 
-    fol = gpmffol(date,sroot,reg);
+    fol = clisatfol(info,date,region);
     @debug "$(Dates.now()) - Moving $(fnc) to data directory $(fol)"
 
-    if isfile("$(fol)/$(fnc)"); @info "$(Dates.now()) - An older version of $(fnc) exists in the $(fol) directory.  Overwriting." end
+    if isfile(joinpath(fol,fnc); @info "$(Dates.now()) - An older version of $(fnc) exists in the $(fol) directory.  Overwriting." end
 
-    mv(fnc,"$(fol)/$(fnc)",force=true);
+    mv(fnc,joinpath(fol,fnc),force=true);
 
 end
