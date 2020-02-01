27b4425c0ff7087362edc84e2ecfb22af973cfff
natgeo-wong <natgeo.wong@outlook.com>
2020-01-31 20:53:07 -0500

    Changed dependency to NCDatasets.jl instead of NetCDF.jl

--- src/ClimateSatellite.jl
+++ src/ClimateSatellite.jl
@@ -5,8 +5,7 @@ module ClimateSatellite
 
 ## Modules Used
 using Dates, Printf, DelimitedFiles
-using NetCDF, HDF5, FTPClient, PyCall
-#using NCDatasets, HDF5, FTPClient, PyCall
+using NCDatasets, HDF5, FTPClient, PyCall
 using ClimateEasy
 
 ## Exporting the following functions:
