361f2d955408f6a782411d554173e44dfa25dbcb
natgeo-wong <natgeo.wong@outlook.com>
2020-02-03 20:19:44 -0500

    Update test functions due to new syntax.

--- test/clisat_test.jl
+++ test/clisat_test.jl
@@ -3,13 +3,13 @@ using ClimateSatellite
 global_logger(ConsoleLogger(stderr,Logging.Info))
 
 user = "natgeo.wong@outlook.com"
-clisatdwn("gpmimerg",Date(2007,1),email=user,regions=["SGP"])
-data,info,grid = clisatextractall("gpmimerg","prcp_rate",
+clisatdownload("gpmimerg",Date(2007,1),email=user,regions=["SGP"])
+data,info,grid = clisatrawall("gpmimerg","prcp_rate",
                              Date(2007,1,3),Date(2007,1,14),
                              region="SGP");
-data,info = clisatextractpoint("gpmimerg","prcp_rate",
+data,info = clisatrawpoint("gpmimerg","prcp_rate",
                                Date(2007,1,3),Date(2007,1,14),
                                coord=[103.4,2.2],region="SGP");
-data,info,grid = clisatextractgrid("gpmimerg","prcp_rate",
+data,info,grid = clisatrawgrid("gpmimerg","prcp_rate",
                                    Date(2007,1,3),Date(2007,1,14),
                                    grid=[2.5,1.9,105,103.4],region="SGP");
