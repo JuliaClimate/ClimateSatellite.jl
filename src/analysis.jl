abffc34da0bdb45a60390ac0c9f9e1028e880e09
natgeo-wong <natgeo.wong@outlook.com>
2020-02-15 22:37:27 -0500

    Added many patches to analysis functions.  Details found in release notes.

--- src/analysis.jl
+++ src/analysis.jl
@@ -20,6 +20,11 @@ as the datasets become larger and larger.
 
 """
 
+function clisatopNaN(f::Function,data::Vector{Int16})
+    data = data[data.!=-32768];
+    if data != []; return f(data); else; return -32768; end
+end
+
 function clisatanalysis(
     productID::AbstractString, yr::Integer;
     varname::AbstractString,
@@ -35,7 +40,6 @@ function clisatanalysis(
 
     regfol = clisatregfol(info,region);
     rawfol = clisatrawfol(info,Date(yr),region);
-    anafol = clisatanafol(info,Date(yr),region);
     cd(rawfol);
 
     @info "$(Dates.now()) - Extracting $(info["source"]) $(info["product"]) data in $(regionfullname(region)) region during $yr ..."
@@ -43,8 +47,14 @@ function clisatanalysis(
     lon,lat = clisatlonlat(info); rlon,rlat,rinfo = regiongridvec(region,lon,lat);
     nlon = length(rlon); nlat = length(rlat); nt = info["dayfreq"]+1; grid = [rlon,rlat];
 
-    davg = zeros(Int16,nlon,nlat,nt+1,13); dstd = zeros(Int16,nlon,nlat,nt+1,13);
-    dmax = zeros(Int16,nlon,nlat,nt+1,13); dmin = zeros(Int16,nlon,nlat,nt+1,13);
+    davg = rand(Int16,nlon,nlat,nt+1,13); dstd = rand(Int16,nlon,nlat,nt+1,13);
+    dmax = rand(Int16,nlon,nlat,nt+1,13); dmin = rand(Int16,nlon,nlat,nt+1,13);
+
+    zavg = rand(Int16,nlat,nt+1,13); zstd = rand(Int16,nlat,nt+1,13);
+    zmax = rand(Int16,nlat,nt+1,13); zmin = rand(Int16,nlat,nt+1,13);
+
+    mavg = rand(Int16,nlon,nt+1,13); mstd = rand(Int16,nlon,nt+1,13);
+    mmax = rand(Int16,nlon,nt+1,13); mmin = rand(Int16,nlon,nt+1,13);
 
     for mo = 1 : 12; ndy = daysinmonth(yr,mo)
 
@@ -53,61 +63,105 @@ function clisatanalysis(
         ds = Dataset(ncraw,"r"); vds = ds[varname];
         raw = reshape(vds.var[:],nlon,nlat,(nt-1),ndy);
 
-        @debug "$(Dates.now()) - Extracting monthly diurnal climatological information ..."
+        @debug "$(Dates.now()) - Extracting hourly information for each month ..."
         for it = 1 : nt-1, ilat = 1 : nlat, ilon = 1 : nlon
 
             rawii = raw[ilon,ilat,it,:]; rawii = rawii[rawii.!=-32768];
-            davg[ilon,ilat,it,mo] = round(Int16,mean(rawii));
-            dstd[ilon,ilat,it,mo] = round(Int16,std(rawii));
-            dmax[ilon,ilat,it,mo] = round(Int16,maximum(rawii));
-            dmin[ilon,ilat,it,mo] = round(Int16,minimum(rawii));
+
+            if rawii != [];
+                davg[ilon,ilat,it,mo] = round(Int16,mean(rawii));
+                dstd[ilon,ilat,it,mo] = round(Int16,std(rawii));
+                dmax[ilon,ilat,it,mo] = round(Int16,maximum(rawii));
+                dmin[ilon,ilat,it,mo] = round(Int16,minimum(rawii));
+            else
+                davg[ilon,ilat,it,mo] = -32768; dstd[ilon,ilat,it,mo] = -32768;
+                dmax[ilon,ilat,it,mo] = -32768; dmin[ilon,ilat,it,mo] = -32768;
+            end
 
         end
 
-        @debug "$(Dates.now()) - Extracting monthly averaged climatological information ..."
-        davg[:,:,nt,mo] = round.(Int16,mean(davg[:,:,1:nt-1,mo],dims=3));
-        dstd[:,:,nt,mo] = round.(Int16,mean(dstd[:,:,1:nt-1,mo],dims=3));
-        dmax[:,:,nt,mo] = round.(Int16,maximum(dmax[:,:,1:nt-1,mo],dims=3));
-        dmin[:,:,nt,mo] = round.(Int16,minimum(dmin[:,:,1:nt-1,mo],dims=3));
-
         @debug "$(Dates.now()) - Permuting days and hours dimensions ..."
-        raw = permutedims(raw,(1,2,4,3)); tmp = zeros(nlon,nlat,ndy);
+        raw = permutedims(raw,(1,2,4,3)); drg = zeros(nlon,nlat,ndy);
+        dmn = zeros(nlon,nlat,ndy);
 
-        @debug "$(Dates.now()) - Extracting diurnal variability information ..."
+        @debug "$(Dates.now()) - Averaging diurnal data into daily data ..."
         for idy = 1 : ndy, ilat = 1 : nlat, ilon = 1 : nlon
 
             tmpii = raw[ilon,ilat,idy,:]; tmpii = tmpii[tmpii.!=-32768];
-            tmp[ilon,ilat,idy] = maximum(tmpii)/2 - minimum(tmpii)/2;
+            if tmpii != [];
+                drg[ilon,ilat,idy] = maximum(tmpii)/2 - minimum(tmpii)/2;
+                dmn[ilon,ilat,idy] = mean(tmpii);
+            else
+                drg[ilon,ilat,idy] = -32768;
+                dmn[ilon,ilat,idy] = -32768;
+            end
 
         end
 
-        @debug "$(Dates.now()) - Extracting monthly diurnal variability information ..."
-        davg[:,:,nt+1,mo] = round.(Int16,mean(tmp,dims=3));
-        dstd[:,:,nt+1,mo] = round.(Int16,std(tmp,dims=3));
-        dmax[:,:,nt+1,mo] = round.(Int16,maximum(tmp,dims=3));
-        dmin[:,:,nt+1,mo] = round.(Int16,minimum(tmp,dims=3));
+        @debug "$(Dates.now()) - Extracting information on monthly climatology and diurnal variability ..."
+        for ilat = 1 : nlat, ilon = 1 : nlon;
+
+            dmnii = dmn[ilon,ilat,:]; dmnii = dmnii[dmnii.!=-32768];
+            drgii = drg[ilon,ilat,:]; drgii = drgii[drgii.!=-32768];
+
+            davg[ilon,ilat,nt,mo] = round.(Int16,mean(dmnii));
+            dstd[ilon,ilat,nt,mo] = round.(Int16,std(dmnii));
+            dmax[ilon,ilat,nt,mo] = round.(Int16,maximum(dmnii));
+            dmin[ilon,ilat,nt,mo] = round.(Int16,minimum(dmnii));
+
+
+            davg[ilon,ilat,nt+1,mo] = round.(Int16,mean(drgii));
+            dstd[ilon,ilat,nt+1,mo] = round.(Int16,std(drgii));
+            dmax[ilon,ilat,nt+1,mo] = round.(Int16,maximum(drgii));
+            dmin[ilon,ilat,nt+1,mo] = round.(Int16,minimum(drgii));
+
+        end
 
     end
 
     @info "$(Dates.now()) - Calculating yearly climatology for $(info["source"]) $(info["product"]) in $(regionfullname(region)) during $yr ..."
-    davg[:,:,:,end] = round.(Int16,mean(davg[:,:,:,1:12],dims=4));
-    dstd[:,:,:,end] = round.(Int16,mean(dstd[:,:,:,1:12],dims=4));
-    dmax[:,:,:,end] = round.(Int16,maximum(dmax[:,:,:,1:12],dims=4));
-    dmin[:,:,:,end] = round.(Int16,minimum(dmin[:,:,:,1:12],dims=4));
+    for it = 1 : nt+1, ilat = 1 : nlat, ilon = 1 : nlon
 
-    clisatanasave([davg,dstd,dmax,dmin],grid,productID,varname,yr,path,region,info);
+        davg[ilon,ilat,it,end] = round.(Int16,clisatopNaN(mean,davg[ilon,ilat,it,1:12]));
+        dstd[ilon,ilat,it,end] = round.(Int16,clisatopNaN(mean,dstd[ilon,ilat,it,1:12]));
+        dmax[ilon,ilat,it,end] = round.(Int16,clisatopNaN(mean,dmax[ilon,ilat,it,1:12]));
+        dmin[ilon,ilat,it,end] = round.(Int16,clisatopNaN(mean,dmin[ilon,ilat,it,1:12]));
+
+    end
+
+    @info "$(Dates.now()) - Calculating zonal-averaged climatology for $(info["source"]) $(info["product"]) in $(regionfullname(region)) during $yr ..."
+    for ilat = 1 : nlat, it = 1 : nt+1, imo = 1 : 13
+        zavg[ilat,it,imo] = round.(Int16,clisatopNaN(mean,davg[:,ilat,it,imo]));
+        zstd[ilat,it,imo] = round.(Int16,clisatopNaN(mean,dstd[:,ilat,it,imo]));
+        zmax[ilat,it,imo] = round.(Int16,clisatopNaN(mean,dmax[:,ilat,it,imo]));
+        zmin[ilat,it,imo] = round.(Int16,clisatopNaN(mean,dmin[:,ilat,it,imo]));
+    end
+
+    @info "$(Dates.now()) - Calculating meridional-averaged climatology for $(info["source"]) $(info["product"]) in $(regionfullname(region)) during $yr ..."
+    for imo = 1 : 13, it = 1 : nt+1, ilon = 1 : nlon
+        mavg[ilon,it,imo] = round.(Int16,clisatopNaN(mean,davg[ilon,:,it,imo]));
+        mstd[ilon,it,imo] = round.(Int16,clisatopNaN(mean,dstd[ilon,:,it,imo]));
+        mmax[ilon,it,imo] = round.(Int16,clisatopNaN(mean,dmax[ilon,:,it,imo]));
+        mmin[ilon,it,imo] = round.(Int16,clisatopNaN(mean,dmin[ilon,:,it,imo]));
+    end
+
+    clisatanasave([davg,dstd,dmax,dmin],[zavg,zstd,zmax,zmin],[mavg,mstd,mmax,mmin],
+                  grid,productID,varname,yr,path,region,info);
 
 end
 
 function clisatanasave(
-    data::Array{Array{Int16,4},1}, grid::Vector{<:Any},
+    data::Array{Array{Int16,4},1},
+    zdata::Array{Array{Int16,3},1},
+    mdata::Array{Array{Int16,3},1},
+    grid::Vector{<:Any},
     productID::AbstractString, varname::AbstractString, yr::Integer,
     path::AbstractString, region::AbstractString, info::Dict
 )
 
     @info "$(Dates.now()) - Saving analysed $(info["source"]) $(info["product"]) data in $(regionfullname(region)) for the year $yr ..."
 
-    fol = clisatanafol(productID,Date(yr),region,path=path)
+    fol = clisatanafol(productID,region,path=path)
     fnc = joinpath(fol,clisatananame(productID,varname,Date(yr),region));
     rlon,rlat = grid; nlon = length(rlon); nlat = length(rlat); nt = info["dayfreq"];
 
@@ -244,206 +298,206 @@ function clisatanasave(
 
     v = defVar(ds,"zonalavg_yearly_mean_climatology",Int16,
                ("latitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+1,end],dims=1),dims=1);
+    v.var[:] = zdata[1][:,nt+1,end];
 
     v = defVar(ds,"zonalavg_yearly_std_climatology",Int16,
                ("latitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+1,end],dims=1),dims=1);
+    v.var[:] = zdata[2][:,nt+1,end];
 
     v = defVar(ds,"zonalavg_yearly_maximum_climatology",Int16,
                ("latitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+1,end],dims=1),dims=1);
+    v.var[:] = zdata[3][:,nt+1,end];
 
     v = defVar(ds,"zonalavg_yearly_minimum_climatology",Int16,
                ("latitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+1,end],dims=1),dims=1);
+    v.var[:] = zdata[4][:,nt+1,end];
 
 
     v = defVar(ds,"zonalavg_yearly_mean_hourly",Int16,
                ("latitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,1:nt,end],dims=1),dims=1);
+    v.var[:] = zdata[1][:,1:nt,end];
 
     v = defVar(ds,"zonalavg_yearly_std_hourly",Int16,
                ("latitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,1:nt,end],dims=1),dims=1);
+    v.var[:] = zdata[2][:,1:nt,end];
 
     v = defVar(ds,"zonalavg_yearly_maximum_hourly",Int16,
                ("latitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,1:nt,end],dims=1),dims=1);
+    v.var[:] = zdata[3][:,1:nt,end];
 
     v = defVar(ds,"zonalavg_yearly_minimum_hourly",Int16,
                ("latitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,1:nt,end],dims=1),dims=1);
+    v.var[:] = zdata[4][:,1:nt,end];
 
 
     v = defVar(ds,"zonalavg_yearly_mean_diurnalvariance",Int16,
                ("latitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+2,end],dims=1),dims=1);
+    v.var[:] = zdata[1][:,nt+2,end];
 
     v = defVar(ds,"zonalavg_yearly_std_diurnalvariance",Int16,
                ("latitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+2,end],dims=1),dims=1);
+    v.var[:] = zdata[2][:,nt+2,end];
 
     v = defVar(ds,"zonalavg_yearly_maximum_diurnalvariance",Int16,
                ("latitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+2,end],dims=1),dims=1);
+    v.var[:] = zdata[3][:,nt+2,end];
 
     v = defVar(ds,"zonalavg_yearly_minimum_diurnalvariance",Int16,
                ("latitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+2,end],dims=1),dims=1);
+    v.var[:] = zdata[4][:,nt+2,end];
 
 
     v = defVar(ds,"zonalavg_monthly_mean_climatology",Int16,
                ("latitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+1,1:12],dims=1),dims=1);
+    v.var[:] = zdata[1][:,nt+1,1:12];
 
     v = defVar(ds,"zonalavg_monthly_std_climatology",Int16,
                ("latitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+1,1:12],dims=1),dims=1);
+    v.var[:] = zdata[2][:,nt+1,1:12];
 
     v = defVar(ds,"zonalavg_monthly_maximum_climatology",Int16,
                ("latitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+1,1:12],dims=1),dims=1);
+    v.var[:] = zdata[3][:,nt+1,1:12];
 
     v = defVar(ds,"zonalavg_monthly_minimum_climatology",Int16,
                ("latitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+1,1:12],dims=1),dims=1);
+    v.var[:] = zdata[4][:,nt+1,1:12];
 
 
     v = defVar(ds,"zonalavg_monthly_mean_hourly",Int16,
                ("latitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,1:nt,1:12],dims=1),dims=1);
+    v.var[:] = zdata[1][:,1:nt,1:12];
 
     v = defVar(ds,"zonalavg_monthly_std_hourly",Int16,
                ("latitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,1:nt,1:12],dims=1),dims=1);
+    v.var[:] = zdata[2][:,1:nt,1:12];
 
     v = defVar(ds,"zonalavg_monthly_maximum_hourly",Int16,
                ("latitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,1:nt,1:12],dims=1),dims=1);
+    v.var[:] = zdata[3][:,1:nt,1:12];
 
     v = defVar(ds,"zonalavg_monthly_minimum_hourly",Int16,
                ("latitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,1:nt,1:12],dims=1),dims=1);
+    v.var[:] = zdata[4][:,1:nt,1:12];
 
 
     v = defVar(ds,"zonalavg_monthly_mean_diurnalvariance",Int16,
                ("latitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+2,1:12],dims=1),dims=1);
+    v.var[:] = zdata[1][:,nt+2,1:12];
 
     v = defVar(ds,"zonalavg_monthly_std_diurnalvariance",Int16,
                ("latitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+2,1:12],dims=1),dims=1);
+    v.var[:] = zdata[2][:,nt+2,1:12];
 
     v = defVar(ds,"zonalavg_monthly_maximum_diurnalvariance",Int16,
                ("latitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+2,1:12],dims=1),dims=1);
+    v.var[:] = zdata[3][:,nt+2,1:12];
 
     v = defVar(ds,"zonalavg_monthly_minimum_diurnalvariance",Int16,
                ("latitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+2,1:12],dims=1),dims=1);
+    v.var[:] = zdata[4][:,nt+2,1:12];
 
 
     v = defVar(ds,"meridionalavg_yearly_mean_climatology",Int16,
                ("longitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+1,end],dims=2),dims=2);
+    v.var[:] = mdata[1][:,nt+1,end];
 
     v = defVar(ds,"meridionalavg_yearly_std_climatology",Int16,
                ("longitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+1,end],dims=2),dims=2);
+    v.var[:] = mdata[2][:,nt+1,end];
 
     v = defVar(ds,"meridionalavg_yearly_maximum_climatology",Int16,
                ("longitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+1,end],dims=2),dims=2);
+    v.var[:] = mdata[3][:,nt+1,end];
 
     v = defVar(ds,"meridionalavg_yearly_minimum_climatology",Int16,
                ("longitude",),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+1,end],dims=2),dims=2);
+    v.var[:] = mdata[4][:,nt+1,end];
 
 
     v = defVar(ds,"meridionalavg_yearly_mean_hourly",Int16,
                ("longitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,1:nt,end],dims=2),dims=2);
+    v.var[:] = mdata[1][:,1:nt,end];
 
     v = defVar(ds,"meridionalavg_yearly_std_hourly",Int16,
                ("longitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,1:nt,end],dims=2),dims=2);
+    v.var[:] = mdata[2][:,1:nt,end];
 
     v = defVar(ds,"meridionalavg_yearly_maximum_hourly",Int16,
                ("longitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,1:nt,end],dims=2),dims=2);
+    v.var[:] = mdata[3][:,1:nt,end];
 
     v = defVar(ds,"meridionalavg_yearly_minimum_hourly",Int16,
                ("longitude","hour"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,1:nt,end],dims=2),dims=2);
+    v.var[:] = mdata[4][:,1:nt,end];
 
 
     v = defVar(ds,"meridionalavg_yearly_mean_diurnalvariance",Int16,
                ("longitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+2,end],dims=2),dims=2);
+    v.var[:] = mdata[1][:,nt+2,end];
 
     v = defVar(ds,"meridionalavg_yearly_std_diurnalvariance",Int16,
                ("longitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+2,end],dims=2),dims=2);
+    v.var[:] = mdata[2][:,nt+2,end];
 
     v = defVar(ds,"meridionalavg_yearly_maximum_diurnalvariance",Int16,
                ("longitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+2,end],dims=2),dims=2);
+    v.var[:] = mdata[3][:,nt+2,end];
 
     v = defVar(ds,"meridionalavg_yearly_minimum_diurnalvariance",Int16,
                ("longitude",),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+2,end],dims=2),dims=2);
+    v.var[:] = mdata[4][:,nt+2,end];
 
 
     v = defVar(ds,"meridionalavg_monthly_mean_climatology",Int16,
                ("longitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+1,1:12],dims=2),dims=2);
+    v.var[:] = mdata[1][:,nt+1,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_std_climatology",Int16,
                ("longitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+1,1:12],dims=2),dims=2);
+    v.var[:] = mdata[2][:,nt+1,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_maximum_climatology",Int16,
                ("longitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+1,1:12],dims=2),dims=2);
+    v.var[:] = mdata[3][:,nt+1,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_minimum_climatology",Int16,
                ("longitude","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+1,1:12],dims=2),dims=2);
+    v.var[:] = mdata[4][:,nt+1,1:12];
 
 
     v = defVar(ds,"meridionalavg_monthly_mean_hourly",Int16,
                ("longitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[1][:,:,1:nt,1:12],dims=2),dims=2);
+    v.var[:] = mdata[1][:,1:nt,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_std_hourly",Int16,
                ("longitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[2][:,:,1:nt,1:12],dims=2),dims=2);
+    v.var[:] = mdata[2][:,1:nt,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_maximum_hourly",Int16,
                ("longitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[3][:,:,1:nt,1:12],dims=2),dims=2);
+    v.var[:] = mdata[3][:,1:nt,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_minimum_hourly",Int16,
                ("longitude","hour","month"),attrib=att_var[1]);
-    v.var[:] = dropdims(mean(data[4][:,:,1:nt,1:12],dims=2),dims=2);
+    v.var[:] = mdata[4][:,1:nt,1:12];
 
 
     v = defVar(ds,"meridionalavg_monthly_mean_diurnalvariance",Int16,
                ("longitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[1][:,:,nt+2,1:12],dims=2),dims=2);
+    v.var[:] = mdata[1][:,nt+2,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_std_diurnalvariance",Int16,
                ("longitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[2][:,:,nt+2,1:12],dims=2),dims=2);
+    v.var[:] = mdata[2][:,nt+2,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_maximum_diurnalvariance",Int16,
                ("longitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[3][:,:,nt+2,1:12],dims=2),dims=2);
+    v.var[:] = mdata[3][:,nt+2,1:12];
 
     v = defVar(ds,"meridionalavg_monthly_minimum_diurnalvariance",Int16,
                ("longitude","month"),attrib=att_var[2]);
-    v.var[:] = dropdims(mean(data[4][:,:,nt+2,1:12],dims=2),dims=2);
+    v.var[:] = mdata[4][:,nt+2,1:12];
 
     close(ds);
 
